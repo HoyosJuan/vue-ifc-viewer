1a7558594f29ed201c889d38055f0975b6b6f553
Juan Hoyos <juan.hoyos4@gmail.com>
2023-12-21 17:49:27 -0500

    Simple example in src

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <IFCViewer />
+</template>
+
+<script setup lang="ts">
+import IFCViewer from '../../lib'
+import * as OBC from "openbim-components"
+import { onMounted } from 'vue';
+import { useViewersManager } from '../../lib'
+
+const viewersManager = useViewersManager()
+const viewer = viewersManager.get()
+
+onMounted(() => {
+  const ifcLoader = viewer.tools.get(OBC.FragmentIfcLoader)
+  ifcLoader.onIfcLoaded.add(model => {
+    console.log(`${model.name} loaded!`)
+  })
+})
+</script>
+
+<style scoped></style>
